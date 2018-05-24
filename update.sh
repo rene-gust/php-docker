07e1c7874060b2f27e9c7740fe68a5c819c155f1
Linus Lotz <l.lotz@reply.de>
2018-05-24 16:36:13 +0200

    add bash per default

--- update.sh
+++ update.sh
@@ -56,7 +56,7 @@ do
     pecl_extensions=""
     php_extensions=""
     build_deps=""
-    run_deps=""
+    run_deps="bash "
     for ext in $(cat ${DIR}/${framework}/deps)
     do
         add_extension $ext
